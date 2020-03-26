3601d2b69b821217d5018eef91e82fb68b827668
Lynchmaniac <lynchmaniac@gmail.com>
2020-03-26 17:17:19 +0100

    feat: add lumos example

---
+++ python/examples/lumos.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# NeoPixel library strandtest example
+# Author: Tony DiCola (tony@tonydicola.com)
+#
+# Direct port of the Arduino NeoPixel library strandtest example.  Showcases
+# various animations on a strip of NeoPixels.
+
+import time
+from neopixel import *
+import argparse
+
+# LED strip configuration:
+LED_COUNT      = 7      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+
+
+# Define functions which animate LEDs in various ways.
+def colorWipe(strip, color, brightness=255, wait_ms=50):
+    """Wipe color across display a pixel at a time."""
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.setBrightness(brightness)
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+
+
+
+
+# Main program logic follows:
+if __name__ == '__main__':
+    # Process arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
+    args = parser.parse_args()
+
+    # Create NeoPixel object with appropriate configuration.
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+
+    print ('Press Ctrl-C to quit.')
+    if not args.clear:
+        print('Use "-c" argument to clear LEDs on exit')
+
+    try:
+
+        print ('Color wipe animations.')
+        timelapse = 1/7
+        colorWipe(strip, Color(50, 255, 255),50,0)      # Purple wipe
+        time.sleep(timelapse)
+        colorWipe(strip, Color(100, 255, 255),100,0)    # Purple wipe
+        time.sleep(timelapse)
+        colorWipe(strip, Color(150, 255, 255),150,0)    # Purple wipe
+        time.sleep(timelapse)
+        colorWipe(strip, Color(200, 255, 255),200,0)    # Purple wipe
+        time.sleep(timelapse)
+        colorWipe(strip, Color(255, 255, 255),255,0)    # White wipe
+        time.sleep(1)
+        colorWipe(strip, Color(255, 255, 0),255,0)      # Yellow wipe
+        time.sleep(1)
+        colorWipe(strip, Color(200, 200, 200),200)      # White wipe
+        time.sleep(timelapse)
+        colorWipe(strip, Color(150, 150, 150),150)      # White wipe
+        time.sleep(timelapse)
+        colorWipe(strip, Color(100, 100, 100),100)      # White wipe
+        time.sleep(timelapse)
+        colorWipe(strip, Color(50, 50, 50),50)          # White wipe
+        time.sleep(timelapse)
+        colorWipe(strip, Color(0,0,0), 50, 10)
+
+
+    except KeyboardInterrupt:
+        if args.clear:
+            colorWipe(strip, Color(0,0,0), 10)
